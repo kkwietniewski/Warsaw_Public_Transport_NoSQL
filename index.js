02aa8ec9da269d6c31c6b7b4cbdd176604084f9e
Monika Nogała <mxnogala@gmail.com>
2021-01-11 14:32:11 +0100

    endpoints records and record

--- index.js
+++ index.js
@@ -1,21 +1,63 @@
 const {runServer, app} = require('./server') ;
+const cypher = require('cypher-query-builder');
 const neo4j = require('neo4j-driver'); 
 const driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', '123'));
 const session = driver.session();
-
+let db = new cypher.Connection('bolt://localhost', {username: 'neo4j', password: '123',});
 runServer(3000) ;
 
 app.get("/", (req, res)=> {
-   session
-    .run('MATCH (n) RETURN n')
-        .then(function(result) {
-            result.records.forEach(function(record) {
-                console.log(record._fields[0].properties);
-                
-            });
-        })
-        .catch(function(err){
-            console.log(err);
-        });
-        res.send('fuck it'); 
+
+    res.send("Hello public transport!"); 
 });
+
+app.get("/records", async (req,res)=> {
+    let records = [];
+    const results = await db.matchNode('n')
+    .return('n')
+    .run();
+ 
+    records = results.map(row=>row.n.properties.name); 
+    console.log(records); 
+
+});
+
+app.get("/record/:name", async (req, res)=>
+{
+   let name = req.params.name;
+   const results = await db.matchNode('line', 'Line')
+   .where({'line.name':name})
+   .return('line') 
+   .run();
+
+   let record = results.map(row=>row.line.properties.name);
+   console.log(record); 
+
+});
+
+app.post("/create", async (req, res)=>
+{
+    let formName = req.body.name;
+    // const results = await db.create('line', formName, {name:formName})
+    // .
+
+});
+
+
+ // try {
+    //     const result = await session.run("MATCH (n) return n");
+    
+    //     const nodes = result.records;
+    
+    //     nodes.forEach((record) => {
+    //       records.push(record.get(0));
+    //     });
+    
+    //     records.forEach((result) => {
+    //       console.log(result.properties.name);
+    //     });
+    //   } finally {
+    //     await session.close();
+    //   }
+    //   res.send(records);
+    //   await driver.close();
