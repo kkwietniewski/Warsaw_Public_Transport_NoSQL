fd44c803ca6f868794fdf89e721e8b568aefd208
kkwietniewski <kkwietniewski@edu.cdv.pl>
2021-01-11 14:52:00 +0100

    endpoint create

--- index.js
+++ index.js
@@ -1,63 +1,64 @@
-const {runServer, app} = require('./server') ;
-const cypher = require('cypher-query-builder');
-const neo4j = require('neo4j-driver'); 
-const driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', '123'));
+const { runServer, app } = require("./server");
+const cypher = require("cypher-query-builder");
+const neo4j = require("neo4j-driver");
+const driver = neo4j.driver(
+  "bolt://localhost",
+  neo4j.auth.basic("neo4j", "123")
+);
 const session = driver.session();
-let db = new cypher.Connection('bolt://localhost', {username: 'neo4j', password: '123',});
-runServer(3000) ;
+let db = new cypher.Connection("bolt://localhost", {
+  username: "neo4j",
+  password: "123",
+});
+runServer(3000);
 
-app.get("/", (req, res)=> {
-
-    res.send("Hello public transport!"); 
+app.get("/", (req, res) => {
+  res.send("Hello public transport!");
 });
 
-app.get("/records", async (req,res)=> {
-    let records = [];
-    const results = await db.matchNode('n')
-    .return('n')
+app.get("/records", async (req, res) => {
+  let records = [];
+  const results = await db.matchNode("n").return("n").run();
+
+  records = results.map((row) => row.n.properties.name);
+  res.send(records);
+});
+
+app.get("/record/:name", async (req, res) => {
+  let name = req.params.name;
+  const results = await db
+    .matchNode("line", "Line")
+    .where({ "line.name": name })
+    .return("line")
     .run();
- 
-    records = results.map(row=>row.n.properties.name); 
-    console.log(records); 
 
+  let record = results.map((row) => row.line.properties.name);
+  res.send(record);
 });
 
-app.get("/record/:name", async (req, res)=>
-{
-   let name = req.params.name;
-   const results = await db.matchNode('line', 'Line')
-   .where({'line.name':name})
-   .return('line') 
-   .run();
-
-   let record = results.map(row=>row.line.properties.name);
-   console.log(record); 
-
+app.post("/create", async (req, res) => {
+  let formName = req.body.name;
+  const results = await db
+    .create(cypher.node("line", "Line", { name: formName }))
+    .return("line")
+    .run();
+  res.send(results.map((row) => row.line.properties.name));
 });
 
-app.post("/create", async (req, res)=>
-{
-    let formName = req.body.name;
-    // const results = await db.create('line', formName, {name:formName})
-    // .
+// try {
+//     const result = await session.run("MATCH (n) return n");
 
-});
+//     const nodes = result.records;
 
+//     nodes.forEach((record) => {
+//       records.push(record.get(0));
+//     });
 
- // try {
-    //     const result = await session.run("MATCH (n) return n");
-    
-    //     const nodes = result.records;
-    
-    //     nodes.forEach((record) => {
-    //       records.push(record.get(0));
-    //     });
-    
-    //     records.forEach((result) => {
-    //       console.log(result.properties.name);
-    //     });
-    //   } finally {
-    //     await session.close();
-    //   }
-    //   res.send(records);
-    //   await driver.close();
+//     records.forEach((result) => {
+//       console.log(result.properties.name);
+//     });
+//   } finally {
+//     await session.close();
+//   }
+//   res.send(records);
+//   await driver.close();
